cced23f18dec207467b0a89f1654677ba15e7b26
KimTaewan <158536479+naughtycoder95@users.noreply.github.com>
2024-03-28 17:41:46 +0900

    study

---
+++ 07_Backend/02_Backend/05_JpaCustomExam/JpaCustomExam/src/test/java/org/example/jpacustomexam/JpaCustomExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpacustomexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCustomExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
